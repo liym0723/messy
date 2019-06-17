c44b65f762acddf7e3c6e1f7d3d07f39114de15b
liym <15079447645@163.com>
2019-06-17 08:35:27 +0800

    20190617 commit

--- messy/app/controllers/users_controller.rb
+++ messy/app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
-
+  include Rails.application.roures.url_helpers
   def index
+    respond_to  do |format|
+      format.html{'xxx'}
+      format.json { render json: "xxx" }
+    end
+  end
+
+  def show
+
   end
 
 
